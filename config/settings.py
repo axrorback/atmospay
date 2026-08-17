4aba95a73ee61a0a116d0a24dfe8246e653776a9
axrorback <axrorback@gmail.com>
2026-08-17 22:44:37 +0500

    Expand REST framework configuration to include parser classes

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': {
-        'json': 'rest_framework.renderers.JSONRenderer',
-    }
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
 }
 
 
