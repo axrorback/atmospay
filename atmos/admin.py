618210d7770adfeac8f0733dfd8b306803706b16
axrorback <axrorback@gmail.com>
2026-08-17 22:30:25 +0500

    Register `Order` and `OrderItem` models in admin with custom list displays

--- atmos/admin.py
+++ atmos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order , OrderItem
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('account', 'status', 'amount', 'created_at')
+
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'items_id', 'code', 'name', 'amount', 'quantity', 'package_code', 'mark_code', 'tin', 'discount')
