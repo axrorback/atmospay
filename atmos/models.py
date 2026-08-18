a8709baa1650eefbfb77c1b193acc8e7636e0aab
axrorback <axrorback@gmail.com>
2026-08-18 13:52:07 +0500

    Update `updated_at` field in `Order` model to use `auto_now` for accurate timestamps

--- atmos/models.py
+++ atmos/models.py
@@ -21,8 +21,7 @@ class Order(models.Model):
     transaction_time = models.CharField(max_length=255, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"Order {self.account} - {self.status}"
 
