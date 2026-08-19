76fb7aa766cb9cd6c4cbf7c6f4b5dbe9e7d066d6
axrorback <axrorback@gmail.com>
2026-08-19 20:11:32 +0500

    Refactor serializers to improve readability, switch to bulk creation for `OrderItem`, and enhance `AtmosCallbackSerializer` field types

--- atmos/serializers.py
+++ atmos/serializers.py
@@ -1,34 +1,77 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+
+from .models import Order
+from .models import OrderItem
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = OrderItem
-        fields = ['items_id', 'code', 'name', 'amount', 'quantity', 'package_code', 'mark_code', 'tin', 'discount']
+
+        fields = (
+            "items_id",
+            "code",
+            "name",
+            "amount",
+            "quantity",
+            "package_code",
+            "mark_code",
+            "tin",
+            "discount",
+        )
 
 
 class CreateOrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+
+    items = OrderItemSerializer(
+        many=True
+    )
 
     class Meta:
         model = Order
-        fields = ['account', 'amount', 'items']
+
+        fields = (
+            "account",
+            "amount",
+            "items",
+        )
 
     def create(self, validated_data):
-        items_data = validated_data.pop('items')
-        order = Order.objects.create(**validated_data)
 
-        for item_data in items_data:
-            OrderItem.objects.create(order=order, **item_data)
+        items = validated_data.pop(
+            "items"
+        )
+
+        order = Order.objects.create(
+            **validated_data
+        )
+
+        OrderItem.objects.bulk_create(
+            [
+                OrderItem(
+                    order=order,
+                    **item
+                )
+                for item in items
+            ]
+        )
 
         return order
 
 
-class AtmosCallbackSerializer(serializers.Serializer):
+class AtmosCallbackSerializer(
+    serializers.Serializer
+):
+
     store_id = serializers.IntegerField()
+
     transaction_id = serializers.CharField()
-    transaction_time = serializers.CharField()
+
+    transaction_time = serializers.DateTimeField()
+
     amount = serializers.IntegerField()
-    invoice = serializers.CharField()
+
+    invoice = serializers.IntegerField()
+
     sign = serializers.CharField()
